3d55f048ce1fab3c75c4fc36b2a1df0442896b81
Lorna <eleonora.dups@gmail.com>
2019-09-27 14:33:10 +0200

    ajout do while

--- Exercice5.1/main.cpp
+++ Exercice5.1/main.cpp
@@ -1,42 +1,72 @@
 #include <iostream>
+#include <time.h>
+
+
+// exercice 5.1
+
+
+
+/* INIT
+Cette partie doit donner une valeur à mysteryNumber comprise entre 0 et 100
+			
+PLAY
+Cette partie doit:
+	- essayer de deviner le nombre mystère à l'aide d'un function
+	- Si le nombre mystere est trouvé il faut passer à l'état END
+			
+END			
+Cette partie doit afficher le nombre de coups qui ont été nécessaire pour trouver le nombre mystère et indiquer quel est ce nombre mystère
+*/
+
 
 enum class GameState {
+
 	INIT,
 	PLAY,
 	END,
 	EXIT
+
 };
 
-void GenerateMysteryNumber(int& mystery) {
-	
+int GenerateMysteryNumber(int& mystery) {
+	mystery = rand() % 101;
+	return mystery;
 }
 
 int main() {
-	GameState gameState = GameState::INIT;
 
+	
+	GameState gameState = GameState::INIT;
 	unsigned int mysteryNumber;
+	int nbrTurns = 0;
+	
+	srand(time(NULL));
 
 	while (gameState != GameState::EXIT);
 	{
-		switch (gameState) {
+		
+		std::cout << "Enter a number between 0 and 100";
+		std::cin >> mysteryNumber;
+		
+		GenerateMysteryNumber;
+
+		switch (gameState) 
+		{
 		case GameState::INIT:
-			/*
-				Cette partie doit donner une valeur à mysterNumber comprise entre 0 et 100
-			*/
+
+			
 			gameState = GameState::PLAY;
 			break;
 
 		case GameState::PLAY:
 			/*
-				Cette partie doit:
-				- essayer de deviner le nombre mystère à l'aide d'un function
-				- Si le nombre mystre est trouvé il faut passer à l'état END
+				
 			*/
 			break;
 
 		case GameState::END:
 			/*
-				Cette partie doit afficher le nombre de coups qui ont été nécessaire pour trouver le nombre mystère et indiquer quel est ce nombre mystère
+				
 			*/
 			break;
 
@@ -46,6 +76,6 @@ int main() {
 	}
 
 	
-	system("pose");
-	return EXIT_FAILURE;
+	system("pause");
+	return EXIT_SUCCESS;
 }
