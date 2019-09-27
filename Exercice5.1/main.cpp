4470ad694a0d7b62fb141f9ae79bcb6b90b1d45f
Lorna <eleonora.dups@gmail.com>
2019-09-27 15:46:36 +0200

    commentaire enlever

--- Exercice5.1/main.cpp
+++ Exercice5.1/main.cpp
@@ -6,21 +6,6 @@
 
 
 
-/* INIT
-Cette partie doit donner une valeur à mysteryNumber comprise entre 0 et 100
-			
-PLAY
-Cette partie doit:
-	- essayer de deviner le nombre mystère à l'aide d'un function
-	- Si le nombre mystere est trouvé il faut passer à l'état END
-			
-END			
-Cette partie doit afficher le nombre de coups qui ont été nécessaire pour trouver le nombre mystère et indiquer quel est ce nombre mystère
-*/
-
-
-
-
 int main() {
 	
 
